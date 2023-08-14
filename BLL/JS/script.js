27b5554a3da2d5c2da6b47930206ecdccaa650bb
Diego <diegoarcecast@gmail.com>
2023-08-13 23:17:05 -0600

    nom2

---
+++ BLL/JS/script.js
@@ -0,0 +1,8 @@
+window.alert("JS CONECTADO")
+
+element.addEventListener("click", function(){ window.open('C:\Users\diego\OneDrive\Documents\Portafolio\PL\index.html') });
+
+
+
+
+document.getElementById("btnHome").addEventListener("click", function(){ window.open("https://www.w3schools.com") });
