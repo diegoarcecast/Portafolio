57d5b72e322e4a8fce30a82b59831f963d4b1ae5
Diego <diegoarcecast@gmail.com>
2023-08-16 15:19:15 -0600

    html_files

--- BLL/JS/script.js
+++ BLL/JS/script.js
@@ -1,8 +1,33 @@
 window.alert("JS CONECTADO")
 
-element.addEventListener("click", function(){ window.open('C:\Users\diego\OneDrive\Documents\Portafolio\PL\index.html') });
+var reply_click_about = function()
+{
+    //alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+    window,open('about.html');
+}
+var reply_click_services = function()
+{
+    //alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+    window,open('services.html');
+}
+var reply_click_portfolio = function()
+{
+    //alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+    window,open('portfolio.html');
+}
+var reply_click_home = function()
+{
+    //alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+    window,open('index.html');
+}
+var reply_click_contact = function()
+{
+    //alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+    window,open('contact.html');
+}
 
-
-
-
-document.getElementById("btnHome").addEventListener("click", function(){ window.open("https://www.w3schools.com") });
+document.getElementById('btnHome').onclick = reply_click_home;
+document.getElementById('btnAbout').onclick = reply_click_about;
+document.getElementById('btnServices').onclick = reply_click_services;
+document.getElementById('btnPortfolio').onclick = reply_click_portfolio;
+document.getElementById('btnContact').onclick = reply_click_contact;
